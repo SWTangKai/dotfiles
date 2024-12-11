3641823fcbd330927b8a025c22b68a424bd9fca1
Tang Kai <tangkailh@outlook.com>
2024-12-11 19:39:42 +0800

    feat: add installsh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+wget https://github.com/MordechaiHadad/bob/releases/latest/download/bob-linux-x86_64.zip
+
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
